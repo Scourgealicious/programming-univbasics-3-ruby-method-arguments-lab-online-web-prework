637c8af0a9fd7fa7b4922786d62f404804cfe6ee
Kevin McMullen <kevin_mcmullen418@yahoo.com>
2019-08-04 22:54:48 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,10 +2,18 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(name, language = "Ruby")
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I'm learning to program in #{language}."
+end
+
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, may name is #{name} and I'm learning to program in #{language}."
 end
 
 introduction("Kevin")
 
 introduction_with_language("Kevin")
+
+introduction_with_language_optional("Kevin")
+
+introduction_with_language_optional("Kevin","Python")
